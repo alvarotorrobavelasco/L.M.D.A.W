cc571bb863adc9751b2a60abc5a077e1f1c3bd9d
alvarotorrobavelasco <atorvel2911@g.educaand.es>
2025-03-25 19:39:24 +0100

    Delete tema3/Ejercicio1 directory

--- tema3/Ejercicio1/ej1js/MyJS.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "";
-  }
-  function myFunction2() {
-    document.getElementById("demo").innerHTML = "Párrafo cambiado.";
-  }
